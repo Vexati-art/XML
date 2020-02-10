91b197d3f0b5d185c97a490aa291db1649d8726c
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-02-10 19:24:44 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,27 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("http://rss.cnn.com/rss/edition.rss");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $top = $data->getElementsByTagName("Top")->item(0)->nodeValue;
+   $name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
+   $score = $data->getElementsByTagName("Score")->item(0)->nodeValue;
+   $team = $data->getElementsByTagName("Team")->item(0)->nodeValue;
+  
+   echo "
+            <ul>
+                <li>Top: <strong>$top</strong></li>
+                <li>Name: <strong>$name</strong></li>
+                <li>Score: <strong>$score</strong></li>
+                <li>Team: <strong>$team</strong></li>
+            </ul>
+        </li>";
+ }
+?>
+</ul>
