9ea3a8d513be275150bbe403aa40a823c31741cf
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-02-10 19:29:39 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,27 +1,21 @@
 <?php
- $domOBJ = new DOMDocument();
- $domOBJ->load("https://scanvenger.herokuapp.com/");//XML page URL
- 
- $content = $domOBJ->getElementsByTagName("item");
- 
- ?>
- <ul>
-    <?php
- foreach( $content as $data )
- {
-   $top = $data->getElementsByTagName("Top")->item(0)->nodeValue;
-   $name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
-   $score = $data->getElementsByTagName("Score")->item(0)->nodeValue;
-   $team = $data->getElementsByTagName("Team")->item(0)->nodeValue;
-  
-   echo "
-            <ul>
-                <li>Top: <strong>$top</strong></li>
-                <li>Name: <strong>$name</strong></li>
-                <li>Score: <strong>$score</strong></li>
-                <li>Team: <strong>$team</strong></li>
-            </ul>
-        </li>";
- }
-?>
-</ul>
+$domOBJ = new DOMDocument();
+$domOBJ->load("http://scanvenger.herokuapp.com/");
+
+$content = $domOBJ->getElementsByTagName("item");
+
+foreach ($content as $data) {
+
+    $top = $data->getElementsByTagName("top")->item(0)->nodeValue;
+    $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+    $score = $data->getElementsByTagName("scorer")->item(0)->nodeValue;
+    $team = $data->getElementsByTagName("team")->item(0)->nodeValue;
+
+    echo "
+    <ul>
+    <li>top: <strong>$top</strong></li>
+    <li>name: <strong>$name</strong></li>
+    <li>score: <strong>$score</strong></li>
+    <li>team: <strong>$team</strong></li>
+    </ul>";
+}
